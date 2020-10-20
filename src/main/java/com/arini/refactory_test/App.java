95aaf4ad06a3d39d7f907e7b920b1bd032bd18c8
Arini Mauluddina <mauluddinaa@gmail.com>
2020-10-20 23:00:46 +0700

    soal nomer 1

---
+++ src/main/java/com/arini/refactory_test/App.java
@@ -0,0 +1,52 @@
+package com.arini.refactory_test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	 public static void main(String[] args) {        
+         
+		 Scanner scan = new Scanner(System.in);
+			int inputLenght = 7; // jumlah data 7 
+			int[] array = new int[7];
+			int temporer,index = 1;
+			
+			for (int i = 0; i < inputLenght; i++) {
+				System.out.print("Masukan angka ke - "+(i+1)+" : ");
+				array[i] = scan.nextInt();
+			}
+			System.out.println();
+			
+			for (int i = 0; i < array.length; i++) {
+				for (int j = i+1; j < array.length; j++) {
+					List<Integer> list = new ArrayList<>();
+					if (array[i] > array[j]) {
+						list.add(array[j]);
+						list.add(array[i]);
+						temporer = array[i];
+						array[i] = array[j];
+						array[j] = temporer;
+						System.out.println((index++)+". "+ list + " -> " + Arrays.toString(array));
+						
+						if(i == 0) {
+							System.out.println();
+							System.out.println("Jumlah swap adalah: "+(index - 1));
+							System.exit(0);
+						}else {
+							i--;
+						}
+						j = i;
+					}else {
+						i++;
+					}
+				}
+			}
+			
+		}
+
+	}
